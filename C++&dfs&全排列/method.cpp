eaa3efd4190db9157228ff9f8530097b72585039
Sivan-w <1975970391@qq.com>
2022-03-17 00:25:32 +0800

    Add files via upload

--- C++&dfs&全排列/method.cpp
+++ C++&dfs&全排列/method.cpp
@@ -2,26 +2,26 @@
 #include<cstdlib>
 using namespace std;
 
-int n;
-int ans[21];
-int mark[20]={0};
+int n;           //求 1~n的全排列
+int ans[20];     //将每次的排列结果存入ans数组
+int mark[20]={0};//标记1~n个数的使用情况，i用mark[i-1]来标记
 
 void dfs(int step)
 {
-    if(step==n+1)
+    if(step==n)  //此时ans[n-1]已存满，输出结果
     {
-        for(int i=0;i<=step;i++)
-            cout<<ans[i];
+        for(int i=0;i<n;i++)
+            cout<<ans[i]<<" ";
         cout<<endl;
     }
-    for(int i=0;i<=n;i++)
+    for(int i=1;i<=n;i++)
     {
-        if(mark[i]==0)
+        if(mark[i-1]==0)  //mark[i-1]标记第i个数，即i
         {
             ans[step]=i;
-            mark[i]=1;
-            dfs(step+1);
-            mark[i]=0;
+            mark[i-1]=1;  //标记
+            dfs(step+1);  //递归入栈
+            mark[i-1]=0;  //回溯法，出栈
         }
     }
 }
@@ -29,6 +29,5 @@ int main()
 {
     cin>>n;
     dfs(0);
-    system("pause");
     return 0;
 }
